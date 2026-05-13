2c5d4c71938262a12e721a6a88efd304ddfe2800
OiJs <oijs9663@gmail.com>
2026-05-13 13:51:45 +0900

    feat: Pageable

--- src/main/java/com/nhnacademy/springbootjpa/controller/GetMemberController.java
+++ src/main/java/com/nhnacademy/springbootjpa/controller/GetMemberController.java
@@ -1,16 +1,27 @@
 package com.nhnacademy.springbootjpa.controller;
 
+import com.nhnacademy.springbootjpa.entity.Member;
 import com.nhnacademy.springbootjpa.entity.MemberNameOnlyView;
+import com.nhnacademy.springbootjpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class GetMemberController {
-
+    private final MemberRepository memberRepository;
     // TODO #1: `GET /members?page=2&size=3` 와 같은 요청을 처리하도록 아래 코드를 완성하세요.
     @GetMapping("/members")
-    public Page<MemberNameOnlyView> findAll() {
-        return null;
+    public Page<MemberNameOnlyView> findAll(Pageable pageable) {
+        return memberRepository.findAllBy(pageable);
+    }
+
+    @GetMapping("/membersEntity")
+    public Page<Member> findAllEntity(Pageable pageable) {
+        return memberRepository.findAll(pageable);
     }
 }
